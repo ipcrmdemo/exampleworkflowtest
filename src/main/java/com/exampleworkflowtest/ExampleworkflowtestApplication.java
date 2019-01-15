ae0295090737c0f6271941cf7e8a804df52309f6
Matt Cadorette <mcadorette@gmail.com>
2019-01-15 12:21:11 -0500

    Initial commit from Atomist

---
+++ src/main/java/com/exampleworkflowtest/ExampleworkflowtestApplication.java
@@ -0,0 +1,12 @@
+package com.exampleworkflowtest;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleworkflowtestApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(ExampleworkflowtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampleworkflowtest/ExampleworkflowtestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.exampleworkflowtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleworkflowtestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
